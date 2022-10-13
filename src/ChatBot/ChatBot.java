ea33f7dbb09d8ecda1fbe14946f0d250f633c8df
Savelii Khizhnikov <s.i.khizhnikov@student.khai.edu>
2022-10-13 17:03:15 +0300

    Chatbot 2-nd stage

--- src/ChatBot/ChatBot.java
+++ src/ChatBot/ChatBot.java
@@ -1,8 +1,14 @@
 package src.ChatBot;
 
+import java.util.Scanner;
+
 public class ChatBot {
     public static void main(String[] args){
         System.out.println("Hello! My name is Svinus.");
-        System.out.print("I was created in 2022.");
+        System.out.println("I was created in 2022.");
+        System.out.println("Please remind me your name.");
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        System.out.print("What a great name you have, "+ name + "!");
     }
 }
