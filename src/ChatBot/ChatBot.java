dd7c4264b77a0253d79ccf7c5daf0c96abf0a0f7
Savelii Khizhnikov <s.i.khizhnikov@student.khai.edu>
2022-10-13 16:51:31 +0300

    Chatbot 1-st stage

---
+++ src/ChatBot/ChatBot.java
@@ -0,0 +1,8 @@
+package src.ChatBot;
+
+public class ChatBot {
+    public static void main(String[] args){
+        System.out.println("Hello! My name is Svinus.");
+        System.out.print("I was created in 2022.");
+    }
+}
