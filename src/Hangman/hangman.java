f9c5c08261be2aaf6b8ae5ef33e1226bf9b6047b
Savelii Khizhnikov <s.i.khizhnikov@student.khai.edu>
2022-10-15 19:01:42 +0300

    Hangman 4-th stage

--- src/Hangman/hangman.java
+++ src/Hangman/hangman.java
@@ -12,7 +12,7 @@ public class hangman {
         int arr_number = rn.nextInt(4);
 
         System.out.println("HANGMAN");
-        System.out.print("Guess the word: ");
+        System.out.print("Guess the word " + word_breaking(arr[arr_number]) + ": ");
 
         String answer = sc.nextLine();
 
@@ -21,5 +21,19 @@ public class hangman {
         } else {
             System.out.println("You lost!");
         }
+    }
+
+    public static String word_breaking(String br_word){
+        String[] breaking_word = br_word.split("");
+        String[] word = new String[breaking_word.length];
+
+        for (int i = 0; i < breaking_word.length; i++){
+            if (i <= 1) {
+                word[i] = breaking_word[i];
+            } else {
+                word[i] = "-";
+            }
+        }
+        return String.join("", word);
     }
 }
