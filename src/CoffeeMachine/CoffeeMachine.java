5a5fcbcff95ba77bf201b9d62ef12aaecbd84f9c
Savelii Khizhnikov <s.i.khizhnikov@student.khai.edu>
2022-11-09 19:42:27 +0200

    CoffeeMachine 2.5 stage

--- src/CoffeeMachine/CoffeeMachine.java
+++ src/CoffeeMachine/CoffeeMachine.java
@@ -4,16 +4,41 @@ import java.util.Scanner;
 
 public class CoffeeMachine {
 	private static final Scanner sc = new Scanner(System.in);
+	private static int[] components = new int[3];
 
 	public static void main(String[] args){
-		System.out.println("Write how many cups of coffee you will need:");
-		System.out.print("~ ");
+		System.out.print("Write how many ml of water the coffee machine has: ");
+		int water = sc.nextInt();
+		components[0] = Math.floor(water / 200);
 
-		long cups = sc.nextLong();
+		System.out.print("Write how many ml of milk the coffee machine has: ");
+		int milk = sc.nextInt();
+		components[1] = milk % 50;
 
-		System.out.printf("\nFor %s cups of coffee you will need:", cups);
-		System.out.printf("\n%s ml of water", cups * 200);
-		System.out.printf("\n%s ml of milk", cups * 50);
-		System.out.printf("\n%s g of coffee beans", cups * 15);
+		System.out.print("Write how many grams of coffee beans the coffee machine has: ");
+		int beans = sc.nextInt();
+		components[2] = beans % 15;
+
+		System.out.print("Write how many cups of coffee you will need: ");
+		int cups = sc.nextInt();
+
+		if (cups < min(components)){
+			System.out.printf("Yes, I can make that amount of coffee and even %s more than that", min(components));
+		} else if (cups == min(components)){
+			System.out.print("Yes, I can make that amount of coffee");
+		} else {
+			System.out.printf("No, I can make only %s cups of coffee", min(components));
+		}
+	}
+
+	private static int min(int[] arr){
+		int min = arr[0];
+		for (int i = 1; i < arr.length; i++){
+			if (min < arr[i]){
+				min = arr[i];
+				System.out.println(min);
+			}
+		}
+		return min;
 	}
 }
