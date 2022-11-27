25786a3c476963786dc2b28f812154c9eb4b7b70
Savelii Khizhnikov <s.i.khizhnikov@student.khai.edu>
2022-11-27 13:47:10 +0200

    CoffeeMachine 6-th stage

--- src/CoffeeMachine/CoffeeMachine.java
+++ src/CoffeeMachine/CoffeeMachine.java
@@ -4,17 +4,17 @@ import java.util.Scanner;
 
 public class CoffeeMachine {
 	private static final Scanner sc = new Scanner(System.in);
-	private static int[] components = {400, 540, 120, 9, 550};
+	private int water, milk, beans, cup, money;
 
-	public static void main(String[] args) {
-		while (true) {
-			System.out.println("Write action (buy, fill, take, remaining, exit): ");
-			String answer = sc.nextLine();
-			menu(answer);
-		}
+	public CoffeeMachine (int water_in, int milk_in, int beans_in, int cups_in, int money_in){
+		this.water = water_in;
+		this.milk = milk_in;
+		this.beans = beans_in;
+		this.cup = cups_in;
+		this.money = money_in;
 	}
 
-	public static void menu (String answer) {
+	public  void menu (String answer) {
 		switch (answer){
 			case "buy" -> buy();
 			case "fill" -> fill();
@@ -25,39 +25,39 @@ public class CoffeeMachine {
 		}
 	}
 
-	private static void fill(){
+	private void fill(){
 		System.out.println("Write how many ml of water you want to add: ");
-		int water = sc.nextInt() + components[0];
+		water += sc.nextInt();
 		System.out.println("Write how many ml of milk you want to add: ");
-		int milk = sc.nextInt() + components[1];
+		milk += sc.nextInt();
 		System.out.println("Write how many grams of coffee beans you want to add: ");
-		int coffee = sc.nextInt() + components[2];
+		beans += sc.nextInt();
 		System.out.println("Write how many disposable coffee cups you want to add: ");
-		int cups = sc.nextInt() + components[3];
-		components = new int[]{water, milk, coffee, cups, components[4]};
+		cup += sc.nextInt();
 	}
 
-	private static void remaining(){
+	private void remaining(){
 		System.out.println("The coffee machine has:");
-		System.out.printf("%d of water\n", components[0]);
-		System.out.printf("%d of milk\n", components[1]);
-		System.out.printf("%d of coffee beans\n", components[2]);
-		System.out.printf("%d of disposable cups\n", components[3]);
-		System.out.printf("%d of money\n", components[4]);
+		System.out.printf("%d of water\n", water);
+		System.out.printf("%d of milk\n", milk);
+		System.out.printf("%d of coffee beans\n", beans);
+		System.out.printf("%d of disposable cups\n", cup);
+		System.out.printf("%d of money\n", money);
 	}
 
-	private static void take(){
-		System.out.printf("I gave you %d\n", components[4]);
-		components[4] = 0;
+	private void take(){
+		System.out.printf("I gave you %d\n", money);
+		money = 0;
 	}
 
-	private static void buy(){
-		System.out.println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino: ");
+	private void buy(){
+		System.out.println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back - to menu: ");
 		String input = sc.nextLine();
 		switch (input){
 			case "1" -> espresso();
 			case "2" -> latte();
 			case "3" -> cappuccino();
+			case "back" -> System.out.println("Back to menu");
 			default -> {
 				System.out.println("You have to choose something between 1 and 3");
 				buy();
@@ -65,53 +65,52 @@ public class CoffeeMachine {
 		}
 	}
 
-	private static String component_count_check(int water_c, int milk_c, int beans_c){
-		if (components[0] < water_c){
+	private String component_count_check(int water_c, int milk_c, int beans_c){
+		if (water < water_c){
 			return "Sorry, not enough water!";
-		} else if (components[1] < milk_c){
+		} else if (milk < milk_c){
 			return "Sorry, not enough milk!";
-		} else if (components[2] < beans_c){
+		} else if (beans < beans_c){
 			return "Sorry, not enough coffee beans!";
-		} else if (components[3] < 1){
+		} else if (cup < 1){
 			return "Sorry, not enough cups!";
 		}
 		System.out.println("I have enough resources, making you a coffee!");
 		return "enough";
 	}
 
-	private static void cappuccino(){
+	private void cappuccino(){
 		if (component_count_check(200, 100, 12).equals("enough")) {
-			components[0] -= 200;   	//pick water
-			components[1] -= 100;	    //pick milk
-			components[2] -= 12;	    //pick coffee beans
-			components[3] -= 1;        	//pick cup
-			components[4] += 6;        	//add money
+			water -= 200;   	//pick water
+			milk -= 100;	    //pick milk
+			beans -= 12;	    //pick coffee beans
+			cup -= 1;        	//pick cup
+			money += 6;        	//add money
 		} else {
 			System.out.println(component_count_check(200, 100, 12));
 		}
 	}
 
-	private static void espresso(){
+	private void espresso(){
 		if (component_count_check(250, 0, 16).equals("enough")) {
-			components[0] -= 250;       //pick water
-			components[2] -= 16;       	//pick coffee beans
-			components[3] -= 1;         //pick cup
-			components[4] += 4;         //add money
+			water -= 250;       //pick water
+			beans -= 16;       	//pick coffee beans
+			cup -= 1;         //pick cup
+			money += 4;         //add money
 		} else {
 			System.out.println(component_count_check(250, 0, 16));
 		}
 	}
 
-	private static void latte(){
+	private void latte(){
 		if (component_count_check(350, 75, 20).equals("enough")) {
-			components[0] -= 350;       //pick water
-			components[1] -= 75;        //pick milk
-			components[2] -= 20;        //pick coffee beans
-			components[3] -= 1;         //pick cup
-			components[4] += 7;         //add money
+			water -= 350;       //pick water
+			milk -= 75;        //pick milk
+			beans -= 20;        //pick coffee beans
+			cup -= 1;         //pick cup
+			money += 7;         //add money
 		} else {
 			System.out.println(component_count_check(350, 75, 20));
 		}
 	}
 }
-
---
+++ src/CoffeeMachine/Main.java
@@ -0,0 +1,17 @@
+package src.CoffeeMachine;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		CoffeeMachine coffee_machine = new CoffeeMachine(1000, 1000, 1000, 100, 300);
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Write action (buy, fill, take, remaining, exit):");
+		String answer = sc.nextLine();
+		while (!answer.equals("exit")){
+			coffee_machine.menu(answer);
+			System.out.println("Write action (buy, fill, take, remaining, exit):");
+			answer = sc.nextLine();
+		}
+	}
+}
